dd7170d1b714d3a48536b7f5f1a64f8e91c52696
Erivan Ferreira <erivan.c39@gmail.com>
2021-12-09 21:38:56 -0300

    fix:remove testing buttons

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import GlobalStyles from "./styles/global";
 import { Route } from "react-router-dom";
-import PageButton from "./components/PageButton";
 function App() {
   return (
     <>
       <GlobalStyles />
       <Route />
-      <PageButton left />
-      <PageButton right />
     </>
   );
 }
