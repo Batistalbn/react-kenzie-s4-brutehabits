3882f25d57400c66370d4a7859e609362742d764
Erivan Ferreira <erivan.c39@gmail.com>
2021-12-09 21:37:49 -0300

    feat: create icons for page navgation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import GlobalStyles from "./styles/global";
 import { Route } from "react-router-dom";
+import PageButton from "./components/PageButton";
 function App() {
   return (
     <>
       <GlobalStyles />
       <Route />
+      <PageButton left />
+      <PageButton right />
     </>
   );
 }
