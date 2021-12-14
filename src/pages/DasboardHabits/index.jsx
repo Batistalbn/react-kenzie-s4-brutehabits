08e0ca9f08afd2906bbf93655f9affc8636e89d5
Márcio Alves <alvesmarcio@hotmail.com>
2021-12-14 14:38:54 -0300

    Feature: Dashboard habits.

---
+++ src/pages/DasboardHabits/index.jsx
@@ -0,0 +1,17 @@
+import AddButton from "../../components/AddButton";
+
+const DashboardHabits = () => {
+  return (
+    <main>
+      <section>
+        <div>
+          <h3>Habitos</h3>
+          <AddButton />
+          Search
+        </div>
+        Componente Hábitos
+      </section>
+      <div>Listagem Hábitos</div>
+    </main>
+  );
+};
