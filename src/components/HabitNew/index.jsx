4563d091140284072185cd13d2aa810526ea4708
Thiago Trad <thiago_trad@hotmail.com>
2021-12-16 18:14:33 -0300

    Fix: habits

--- src/components/HabitNew/index.jsx
+++ src/components/HabitNew/index.jsx
@@ -1,17 +1,10 @@
 import { Container } from "./style";
 import { Modal as Standart } from "@mui/material";
 
-import 
-import React from 'react'
+import React from "react";
 
 const HabitNew = () => {
+  return <div></div>;
+};
 
-
-    return (
-        <div>
-            
-        </div>
-    )
-}
-
-export default HabitNew
+export default HabitNew;
