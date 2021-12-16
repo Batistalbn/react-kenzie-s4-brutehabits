ddf145c3c928c84f96e514555febf8fc55192762
Thiago Trad <thiago_trad@hotmail.com>
2021-12-16 18:05:57 -0300

    Feature: Add habit

---
+++ src/components/HabitNew/index.jsx
@@ -0,0 +1,17 @@
+import { Container } from "./style";
+import { Modal as Standart } from "@mui/material";
+
+import 
+import React from 'react'
+
+const HabitNew = () => {
+
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default HabitNew
